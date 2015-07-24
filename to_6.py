d7253f6d6d2e2602a7ba6bc165e2d5285be33fc5
marashli <mr.marashli@gmail.com>
2015-07-24 17:40:44 +0300

    Added not working timer

--- to_6.py
+++ to_6.py
@@ -37,26 +37,32 @@ while True:
     for i in range(len(faces)):
         # faces_data[i] = list(faces[i])
         for j in range(4):
-            if faces_data[i][4] is False:
+            if not faces_data[i][4]:
                 faces_data[i][j] = faces[i][j]
     # print("Found {0} faces!".format(len(faces)))
 
     for i in range(len(faces)):
-        if (abs(faces_data[i][0] - faces[i][0])) < 50 and (abs(faces_data[i][1] - faces[i][1])) < 50:
-            cv2.rectangle(frame, (faces[i][0], faces[i][1]), (faces[i][0]+faces[i][2], faces[i][1]+faces[i][3]),
+        if (abs(faces_data[i][0] - faces[i][0])) < faces_data[i][2] and (abs(faces_data[i][1] - faces[i][1])) < faces_data[i][3]:
+
+            cv2.rectangle(frame,
+                          (faces[i][0], faces[i][1]),
+                          (faces[i][0]+faces[i][2], faces[i][1]+faces[i][3]),
                           (faces_data[i][-5], faces_data[i][-4], faces_data[i][-3], 2))
 
             faces_data[i][-2] += int((time.time() % 10000)) - faces_data[i][-1]
             faces_data[i][-1] = int((time.time() % 10000))
-            
-        faces_data[i][-1] = int((time.time() % 10000))
+        else:
+            cv2.rectangle(frame, (faces[i][0], faces[i][1]), (faces[i][0]+faces[i][2], faces[i][1]+faces[i][3]),
+                          (0, 0, 255, 2))
+        faces_data[i][-1] = int(time.time() % 10000)
+
     cv2.imshow("video", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    if len(faces) > 0:
-        for i in range(len(faces_data)):
-            faces_data[i][4] = True
+
+    for i in range(len(faces_data)):
+        faces_data[i][4] = True
 
     print(faces_data)
 
