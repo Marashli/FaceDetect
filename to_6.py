d9aad5a891cf99ed39d8f25d0639d7d9483cf52c
marashli <mr.marashli@gmail.com>
2015-07-24 13:00:02 +0300

    Add timer

--- to_6.py
+++ to_6.py
@@ -7,7 +7,6 @@ cap = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 faces_data = []
-# faces_color = [134, 59, 214, 68, 45, 185, 67, 12, 81, 74, 208]
 time_now = (time.time() % 10000)
 faces_max = 0
 
@@ -26,36 +25,37 @@ while True:
     if len(faces) > faces_max:
         while faces_max < len(faces):
             faces_data.append([0, 0, 0, 0, False, random.randint(0, 255), random.randint(0, 255),
-                               random.randint(0, 255)])
+                               random.randint(0, 255), 0, int((time.time() % 10000))])
             faces_max += 1
 
-    if len(faces) < 1:
-        if (time.time() % 10000) - time_now > 1:
-            pass
-            # print('Ты продержался всго лишь', int((time.time() % 10000) - time_now), 'секунд')
-        time_now = (time.time() % 10000)
+    # if len(faces) < 1:
+    #    if (time.time() % 10000) - time_now > 1:
+    #        pass
+    #        print('Ты продержался всго лишь', int((time.time() % 10000) - time_now), 'секунд')
+    #    time_now = (time.time() % 10000)
 
     for i in range(len(faces)):
         # faces_data[i] = list(faces[i])
         for j in range(4):
-            if faces_data[i][-4] is False:
+            if faces_data[i][4] is False:
                 faces_data[i][j] = faces[i][j]
     # print("Found {0} faces!".format(len(faces)))
 
     for i in range(len(faces)):
         if (abs(faces_data[i][0] - faces[i][0])) < 50 and (abs(faces_data[i][1] - faces[i][1])) < 50:
             cv2.rectangle(frame, (faces[i][0], faces[i][1]), (faces[i][0]+faces[i][2], faces[i][1]+faces[i][3]),
-                          (faces_data[i][-3], faces_data[i][-2], faces_data[i][-1], 2))
-        else:
-            cv2.rectangle(frame, (faces[i][0], faces[i][1]), (faces[i][0]+faces[i][2], faces[i][1]+faces[i][3]),
-                          (0, 0, 255, 2))
+                          (faces_data[i][-5], faces_data[i][-4], faces_data[i][-3], 2))
+
+            faces_data[i][-2] += int((time.time() % 10000)) - faces_data[i][-1]
+            faces_data[i][-1] = int((time.time() % 10000))
     cv2.imshow("video", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     if len(faces) > 0:
         for i in range(len(faces_data)):
-            faces_data[i][-4] = True
+            faces_data[i][4] = True
+
     print(faces_data)
 
 cap.release()
